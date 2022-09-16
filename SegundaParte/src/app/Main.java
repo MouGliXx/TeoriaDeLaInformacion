b2f6ecb6dd6c8502518813e5e586d07ea4a763c1
MouGliXx <97257192+MouGliXx@users.noreply.github.com>
2022-09-16 19:27:13 -0300

    Segunda parte, hasta inciso a)
    - Separo palabras
    - Obtengo alfabetos
    - Calculo frecuencias
    - Calculo cantidad de información
    - Calculo entropías

--- SegundaParte/src/app/Main.java
+++ SegundaParte/src/app/Main.java
@@ -2,10 +2,11 @@ package app;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        BufferedReader archivo = null;
+        BufferedReader archivo;
         String fileName, datos = null;
 
         fileName = "DatosTP1.txt";
@@ -13,26 +14,64 @@ public class Main {
 
         try {
             archivo = new BufferedReader(new FileReader(url));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        try {
             datos = archivo.readLine();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        ArrayList<String> codigo3C = Modelo2.diferenciaPalabras(datos, 3);
-        ArrayList<String> codigo5C = Modelo2.diferenciaPalabras(datos, 5);
-        ArrayList<String> codigo7C = Modelo2.diferenciaPalabras(datos, 7);
+        /**
+         * Separo los datos en palabras de 3, 5 y 7 caracteres
+         * **/
+        ArrayList<String> datos3C = Modelo2.diferenciaPalabras(datos, 3);
+        ArrayList<String> datos5C = Modelo2.diferenciaPalabras(datos, 5);
+        ArrayList<String> datos7C = Modelo2.diferenciaPalabras(datos, 7);
 
-        System.out.println(datos);
-        System.out.println(codigo3C);
-        System.out.println(codigo5C);
-        System.out.println(codigo7C);
-//        System.out.println(codigo3C.size());
-//        System.out.println(codigo5C.size());
-//        System.out.println(codigo7C.size());
+//        System.out.println(datos3C.size());
+//        System.out.println(datos5C.size());
+//        System.out.println(datos7C.size());
+
+        /**
+         * Obtengo las palabras codigo de la secuencia total
+         * **/
+        ArrayList<String> alfabeto3C = Modelo2.identificaPalabrasCodigo(datos3C);
+        ArrayList<String> alfabeto5C = Modelo2.identificaPalabrasCodigo(datos5C);
+        ArrayList<String> alfabeto7C = Modelo2.identificaPalabrasCodigo(datos7C);
+
+//        System.out.println(alfabeto3C.size());
+//        System.out.println(alfabeto5C.size());
+//        System.out.println(alfabeto7C.size());
+
+        /**
+         * Calculo las frecuencias de cada palabra codigo
+         * **/
+        HashMap<String, Integer> frecuencias3C = Modelo2.calculaFrecuencias(datos3C);
+        HashMap<String, Integer> frecuencias5C = Modelo2.calculaFrecuencias(datos5C);
+        HashMap<String, Integer> frecuencias7C = Modelo2.calculaFrecuencias(datos7C);
+
+//        System.out.println(frecuencias3C);
+//        System.out.println(frecuencias5C);
+//        System.out.println(frecuencias7C);
+
+        /**
+         * Calculo la cantidad de informacion de cada palabra codigo
+         * **/
+        HashMap<String, Double> informacion3C = Modelo2.calculaInformacion(frecuencias3C, datos3C.size());
+        HashMap<String, Double> informacion5C = Modelo2.calculaInformacion(frecuencias5C, datos5C.size());
+        HashMap<String, Double> informacion7C = Modelo2.calculaInformacion(frecuencias7C, datos7C.size());
+
+//        System.out.println(informacion3C);
+//        System.out.println(informacion5C);
+//        System.out.println(informacion7C);
+
+        /**
+         * Calculo la entropia de cada alfabeto con palabras de 3, 5 y 7 caracteres
+         * **/
+        double entropia3C = Modelo2.calculaEntropia(alfabeto3C, informacion3C, frecuencias3C, datos3C.size());
+        double entropia5C = Modelo2.calculaEntropia(alfabeto5C,informacion5C, frecuencias5C, datos5C.size());
+        double entropia7C = Modelo2.calculaEntropia(alfabeto7C,informacion7C, frecuencias7C, datos7C.size());
+
+        System.out.println(entropia3C);
+        System.out.println(entropia5C);
+        System.out.println(entropia7C);
     }
 }
--- SegundaParte/src/app/Modelo2.java
+++ SegundaParte/src/app/Modelo2.java
@@ -1,13 +1,14 @@
 package app;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Modelo2 {
 
     public static ArrayList<String> diferenciaPalabras(String datos, int cantCaracteres) {
-        ArrayList<String> codigo = new ArrayList<>();
+        ArrayList<String> palabras = new ArrayList<>();
         String aux;
-        int i = 0, n = datos.length() % cantCaracteres;
+        int i = 0;
 
         while (i < datos.length()) {
             aux = "";
@@ -15,13 +16,62 @@ public class Modelo2 {
                 if (i + k < datos.length())
                     aux += datos.charAt(i + k);
             }
-            codigo.add(aux);
+            palabras.add(aux);
             i += cantCaracteres;
         }
 
-        if (n != 0)
-            codigo.remove(codigo.size() - 1);
+        if (datos.length() % cantCaracteres != 0)
+            palabras.remove(palabras.size() - 1);
 
-        return codigo;
+        return palabras;
+    }
+
+    //Este metodo por ahi no lo uso
+    public static ArrayList<String> identificaPalabrasCodigo(ArrayList<String> palabras) {
+        ArrayList<String> alfabeto = new ArrayList<>();
+
+        for (String palabra: palabras) {
+            if (!alfabeto.contains(palabra))
+                alfabeto.add(palabra);
+        }
+
+        return alfabeto;
+    }
+
+    public static HashMap<String, Integer> calculaFrecuencias(ArrayList<String> datos) {
+        HashMap<String, Integer> frecuencias = new HashMap<>();
+
+        for (String i : datos) {
+            Integer j = frecuencias.get(i);
+            frecuencias.put(i, (j == null) ? 1 : j + 1);
+        }
+
+        return frecuencias;
+    }
+
+    public static double log2(double x) {
+        return Math.log(x) / Math.log(2);
+    }
+
+    public static HashMap<String, Double> calculaInformacion(HashMap<String, Integer> frecuencias, int total) {
+        HashMap<String, Double> informacion = new HashMap<>();
+
+        frecuencias.forEach((key, fr) -> {
+            double probabilidad = (double) fr / total;
+            informacion.put(key, log2(1 / probabilidad));
+        });
+
+        return informacion;
+    }
+
+    public static double calculaEntropia(ArrayList<String> alfabeto, HashMap<String, Double> informacion, HashMap<String, Integer> frecuencias, int total) {
+        double resultado = 0;
+
+        for (String key : alfabeto) {
+            double probabilidad = (double) frecuencias.get(key) / total;
+            resultado += informacion.get(key) * probabilidad;
+        }
+
+        return resultado;
     }
 }
