d54219cc57a806e1d13091769351d3985afb0cde
matiangelico <102809267+matiangelico@users.noreply.github.com>
2022-11-04 23:13:15 -0300

    Avance en Segunda Parte

--- tp2/src/app/App.java
+++ tp2/src/app/App.java
@@ -1,8 +1,47 @@
 package app;
 
+import modelo.SegundaParte;
+
 public class App {
 
     public static void main (String[] args){
 
+        double[][] matriz = new double[5][3];
+        double[] probPriori = new double[5];
+
+        double[][] m = new double[2][2];
+        double[] v = new double[2];
+        m[0][0]= 2./3.;m[0][1]= 1./3.;
+        m[1][0]= 0.1;m[1][1]= 0.9;
+        v[0]=0.75;v[1]=0.25;
+
+
+        matriz[0][0]=0.3;matriz[0][1]=0.27;matriz[0][2]=0.43;
+        matriz[1][0]=0.36;matriz[1][1]=0.4;matriz[1][2]=0.24;
+        matriz[2][0]=0.3;matriz[2][1]=0.27;matriz[2][2]=0.43;
+        matriz[3][0]=0.27;matriz[3][1]=0.4;matriz[3][2]=0.33;
+        matriz[4][0]=0.3;matriz[4][1]=0.36;matriz[4][2]=0.34;
+
+        probPriori[0]=0.2;probPriori[1]=0.1;probPriori[2]=0.3;probPriori[3]=0.3;probPriori[4]=0.1;
+
+        SegundaParte segundaParte = new SegundaParte(m,v);
+
+        System.out.println("PROBABILIDAD DE SALIDA");
+        segundaParte.mostrarVectorSalida();
+        System.out.println();
+        System.out.println("PROBABILIDAD A POSTERIORI");
+        segundaParte.mostrarProbabilidadPosteriori();
+        System.out.println();
+        System.out.println("PROBABILIDAD DEL SUCESO SIMULTANEO");
+        segundaParte.mostrarProbabilidadSucesoSimultaneo();
+        System.out.println();
+        System.out.println("ENTROPIA A PRIORI "+segundaParte.getEntropiaPriori());
+        System.out.println();
+        System.out.print("ENTROPIA A POSTERIORI");
+        segundaParte.mostrarVectorEntropiaPosteriori();
+        System.out.println();
+        System.out.println("EQUIVOCACION "+segundaParte.getEquivocacion());
+        System.out.println();
+        System.out.println("INFORMACION MUTUA "+segundaParte.getInformacionMutua());
     }
 }
--- tp2/src/modelo/SegundaParte.java
+++ tp2/src/modelo/SegundaParte.java
@@ -1,4 +1,125 @@
 package modelo;
 
 public class SegundaParte {
+
+    private static int i=2;
+    private static int j=2;
+    private double[][] matriz = new double[i][j];
+    private double[] probPriori = new double[i];
+    private double[][] probPosteriori;
+    private double[] probSalida;
+    private double[] probSucesoSimultaneo;
+    private double entropiaPriori;
+    private double[] entropiaPosteriori;
+    private double equivocacion;
+    private double informacionMutua;
+
+
+    public SegundaParte(double[][] matriz, double[] vector){
+        this.matriz = matriz;
+        this.probPriori = vector;
+        this.probSalida = calcProbSalida();
+        this.probPosteriori = calcProbPosteriori();
+        this.probSucesoSimultaneo = calcularProbSucesoSimultaneo();
+        this.entropiaPriori = calcEntropiaPriori();
+        this.entropiaPosteriori = calcEntropiaPosteriori();
+        this.equivocacion = calcularEquivocacion();
+        this.informacionMutua = calcInformacionMutua();
+    }
+
+    public double getEntropiaPriori (){
+        return entropiaPriori;
+    }
+
+    public double getInformacionMutua (){
+        return informacionMutua;
+    }
+    public double getEquivocacion (){
+        return equivocacion;
+    }
+    public double[] getEntropiaPosteriori (){
+        return entropiaPosteriori;
+    }
+
+    private double[] calcProbSalida(){
+        int f,c;
+        double suma ;
+        double [] v = new double[i];
+        for (c = 0 ; c < i ; c++){
+            suma = 0;
+            for (f = 0 ; f < j ; f++){
+                suma += matriz[f][c]*probPriori[f];
+            }
+            v[c] = suma;
+        }
+        return v;
+    }
+
+    private double[][] calcProbPosteriori(){
+        int f,c;
+        double [][] v = new double[i][j];
+        for (c = 0 ; c < i ; c++)
+            for (f = 0 ; f < j ; f++)
+                v[f][c] = matriz[f][c]*probPriori[f]/probSalida[c];
+        return v;
+    }
+    private double[] calcularProbSucesoSimultaneo(){
+        double [] v = new double[i];
+        for (int c = 0 ; c < i ; c++)
+            v[c] =  matriz[c][c]*probPriori[c];
+        return v;
+    }
+
+    private double calcEntropiaPriori(){
+        double entropia = 0;
+        for (int i = 0 ; i < j ; i++)
+            entropia += probPriori[i]*(Math.log(1/probPriori[i])/Math.log(2.));
+        return entropia;
+    }
+
+    private double[] calcEntropiaPosteriori(){
+        double[] entropia = new double[i];
+        double suma ;
+        for (int c = 0 ; c < j ; c++) {
+            suma = 0.;
+            for (int f = 0; f < i; f++)
+                suma += probPosteriori[f][c] * (Math.log(1 / probPosteriori[f][c]) / Math.log(2.));
+            entropia[c]= suma;
+        }
+        return entropia;
+    }
+
+    public double calcularEquivocacion (){
+        double equivocacion = 0;
+        for (int c = 0 ; c < j ; c++)
+            for (int f = 0; f < i; f++)
+                equivocacion += probSalida[f] * entropiaPosteriori[f];
+        return equivocacion;
+    }
+
+    private double calcInformacionMutua(){
+        return  equivocacion*entropiaPriori;
+    }
+
+    public void mostrarVectorEntropiaPosteriori (){
+        for (int c = 0 ; c < i ; c++)
+            System.out.print(entropiaPosteriori[c]+" ");
+    }
+    public void mostrarVectorSalida (){
+        for (int c = 0 ; c < i ; c++)
+                System.out.print(probSalida[c]+" ");
+    }
+
+    public void mostrarProbabilidadSucesoSimultaneo (){
+        for (int c = 0 ; c < i ; c++)
+            System.out.print(probSucesoSimultaneo[c]+" ");
+    }
+
+    public void mostrarProbabilidadPosteriori (){
+        for (int f = 0 ; f < i ; f++) {
+            for (int c = 0; c < i; c++)
+                System.out.print(probPosteriori[f][c] + " ");
+            System.out.println();
+        }
+    }
 }
